e744fff7c2e4a3d4429fb1c39b81bd0270287cd6
DanieleTuttolani <danieletuttolani2@gmail.com>
2023-01-09 15:48:32 +0100

    img bonus

--- script/script.js
+++ script/script.js
@@ -1,9 +1,9 @@
-console.log("vue ok ", Vue);
 
 const app = Vue.createApp({
     data() {
         return{
-            mainMessage : 'ciao boolean'
+            mainMessage : 'ciao boolean',
+            picture : "https://banner2.cleanpng.com/20181202/qpa/kisspng-kermit-the-frog-valentines-day-miss-piggy-the-wal-5c03753df11e87.6880608315437304939876.jpg"
         }
     }
 })
